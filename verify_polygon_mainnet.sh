772f382b70e28ca7cc3a1d0819a7f4f6d4986335
AloeSwap <info@aloeswap.com>
2021-12-31 15:46:33 +0100

    New verify scripts

---
+++ verify_polygon_mainnet.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+  echo ""
+  echo "---------------------------------"
+  echo "Solidity source code verification"
+  echo "---------------------------------"
+  echo ""
+  echo "This script publishes smart contract source code to blockchain explorer."
+  echo ""
+  echo "Usage: $0 [CONTRACT NAME 1] [CONTRACT NAME 2] ..."
+  echo "Example: $0 Token Airdropper Presale"
+  echo ""
+  exit 1
+fi
+truffle run verify $@ --network polygonMainnet
