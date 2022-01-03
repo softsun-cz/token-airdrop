99eb560cd2d197e91ed4066ebf3a4ab04e5ac273
AloeSwap <info@aloeswap.com>
2022-01-03 16:26:07 +0100

    -

--- commit.sh
+++ commit.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-if [ "$#" -eq 0 ]; then
-  echo ""
-  echo "------------------------"
-  echo "Git commit & push script"
-  echo "------------------------"
-  echo ""
-  echo "This script commits changes and pushes it on GitHub."
-  echo ""
-  echo "Usage: $0 \"[SOME COMMENT]\""
-  echo "Example: $0 \"Add README.md\""
-  echo ""
-  exit 1
-fi
+#if [ "$#" -eq 0 ]; then
+#  echo ""
+#  echo "------------------------"
+#  echo "Git commit & push script"
+#  echo "------------------------"
+#  echo ""
+#  echo "This script commits changes and pushes it on GitHub."
+#  echo ""
+#  echo "Usage: $0 \"[SOME COMMENT]\""
+#  echo "Example: $0 \"Add README.md\""
+#  echo ""
+#  exit 1
+#fi
 
 git status
 git add *
 git status
-git commit -m \"$@\"
+git commit -m "-"
 git push
 git status
