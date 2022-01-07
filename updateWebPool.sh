3fe96a4955f8929d471da126574f9d10bf72fda2
AloeSwap <info@aloeswap.com>
2022-01-07 19:46:36 +0100

    Add updateWebPool.sh

---
+++ updateWebPool.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$#" -eq 0 ]; then
+  echo ""
+  echo "-------------------------"
+  echo "Pool contract web changer"
+  echo "-------------------------"
+  echo ""
+  echo "This script changes smart contract address on web source and then builds and deploy it."
+  echo ""
+  echo "Usage: $0 [CONTRACT ADDRESS]"
+  echo "Example: $0 0x1234567890123456789012345678901234567890"
+  echo ""
+  exit 1
+fi
+
+sed -i '/addressPool/c\        addressPool: '\'$1\'',' ./web/src/config.ts
+cd web
+./build.sh
+cd ..
