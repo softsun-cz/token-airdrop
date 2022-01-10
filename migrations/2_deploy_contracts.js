ef09e467757c796c4a5045b36440f5d95104f878
AloeSwap <info@aloeswap.com>
2022-01-10 20:37:55 +0100

    Add Dice

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
 // const Token = artifacts.require("Token");
 // const TokenXUSD = artifacts.require("TokenXUSD");
-const Airdrop = artifacts.require("Airdrop");
-const Presale = artifacts.require("Presale");
-const Pool = artifacts.require("Pool");
+// const Airdrop = artifacts.require("Airdrop");
+// const Presale = artifacts.require("Presale");
+// const Pool = artifacts.require("Pool");
+const Dice = artifacts.require("Dice");
 
 
 module.exports = async function(deployer) {
@@ -10,10 +11,12 @@ module.exports = async function(deployer) {
  // await Token.deployed();
  // await deployer.deploy(TokenXUSD);
  // await TokenXUSD.deployed();
- await deployer.deploy(Airdrop);
- await Airdrop.deployed();
- await deployer.deploy(Presale);
- await Presale.deployed();
- await deployer.deploy(Pool);
- await Pool.deployed();
+ // await deployer.deploy(Airdrop);
+ // await Airdrop.deployed();
+ // await deployer.deploy(Presale);
+ // await Presale.deployed();
+ // await deployer.deploy(Pool);
+ // await Pool.deployed();
+ await deployer.deploy(Dice);
+ await Dice.deployed();
 };
