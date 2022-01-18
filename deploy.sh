5182ac1c7d9fdde403a10d7b5135d027fb2e805b
AloeSwap <info@aloeswap.com>
2022-01-18 02:45:41 +0100

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 BUILD=build/
 LOG=deploy.log
 NETWORKS=`node deploy-networks.js`
+CONTRACTS=`node deploy-contracts.js`
 echo ''
 echo '---------------------------'
 echo 'List of available networks:'
 echo '---------------------------'
 echo ''
-parser=' ' read -r -a array <<< $NETWORKS
 ARRAY=($NETWORKS)
 for i in "${!ARRAY[@]}"
 do
@@ -34,3 +34,42 @@ if [ -d "$BUILD" ]; then
  rm -r $BUILD
 fi
 truffle deploy --network $NETWORK 2>&1 | tee $LOG
+
+ARRAY=($CONTRACTS)
+sw=false
+for i in "${!ARRAY[@]}"
+do
+ if [ $sw = false ]; then
+  NAME=${ARRAY[$i]}
+  VERIFY="$VERIFY $NAME"
+  sw=true
+ else
+  sw=false
+ fi
+done
+truffle run verify $VERIFY --network $NETWORK | tee -a $LOG
+
+sw=false
+for i in "${!ARRAY[@]}"
+do
+ if [ $sw = false ]; then
+  NAME=${ARRAY[$i]}
+  ADDR=${ARRAY[$i+1]}
+  echo 'Change address of '$NAME' contract in web config (Y/N, default: N):'
+  read CHANGE
+  if [ "$CHANGE" = 'Y' ] || [ "$CHANGE" = 'y'  ]; then
+   sed -i '/address'$NAME'/c\        address'$NAME': '\'$ADDR\'',' ./web/src/config.ts
+  fi
+  sw=true
+ else
+  sw=false
+ fi
+done
+
+echo 'Run web build script? (Y/N, default: N):'
+read BUILD
+if [ "$BUILD" = 'Y' ] || [ "$BUILD" = 'y'  ]; then
+ cd web
+ ./build.sh
+ cd ..
+fi
