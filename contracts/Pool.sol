9d6683a63730a84619d1d91dcbf2743fdea5094f
AloeSwap <info@aloeswap.com>
2022-01-21 23:08:34 +0100

    Pool update

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -5,121 +5,150 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-
-// TODO: zjistit, k cemu je u eventu indexed
-// TODO: nize dalsi TODO
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 contract Pool is Ownable, ReentrancyGuard {
-    PoolInfo public pool; // Info of our pool
-    mapping(address => UserInfo) public users; // Info of each user that stakes tokens.
-    mapping(address => PoolInfo) public pools; // Info of each user that stakes tokens.
-    uint256 public startBlock; // The block number when token staking starts.
-    event eventDeposit(address indexed user, uint256 poolID, uint256 amount);
-    event eventWithdraw(address indexed user, uint256 poolID, uint256 amount);
-    event eventEmergencyWithdraw(address indexed user, uint256 poolID, uint256 amount);
+    using SafeERC20 for IERC20;
+    mapping(uint256 => mapping(address => UserInfo)) public users; // Info of each user that stakes tokens.
+    PoolInfo[] public pools; // Info of each user that stakes tokens.
+    address public devFeeAddress;
+    bool started = false;
+    event eventDeposit(address indexed user, uint256 indexed poolID, uint256 amount);
+    event eventWithdraw(address indexed user, uint256 indexed poolID, uint256 amount);
+    event eventEmergencyWithdraw(address indexed user, uint256 indexed poolID, uint256 amount);
+    event eventSetDevFeeAddress(address indexed user, address indexed devFeeAddress);
 
     struct UserInfo {
-        uint256 amount;           // How many tokens the user has provided.
-        uint256 rewardDebt;       // Reward debt. See explanation below.
+        uint256 amount; // How many tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
     }
 
     struct PoolInfo {
         IERC20 tokenDeposit;
         IERC20 tokenEarn;
-        uint256 lastRewardBlock;  // Last block number that Tokens distribution occurs.
+        uint256 tokensEarnPerBlock;
+        uint256 lastRewardBlock; // Last block number that Tokens distribution occurs.
         uint256 accTokenPerShare; // Accumulated tokens per share, times 10**12. See below.
+        uint256 fee;
     }
 
-    constructor(uint256 _startBlock) {
-        startBlock = _startBlock;
-    }
-
-    function createPool(address _tokenDepositAddress, address _tokenEarnAddress, uint256 _tokensEarnPerBlock, uint16 _fee) public onlyOwner {
-        pool = PoolInfo();
-        pool.tokenDeposit = IERC20(_tokenDepositAddress);
-        pool.tokenEarn = IERC20(_tokenEarnAddress);
-        pool.tokensEarnPerBlock = _tokensEarnPerBlock;
-        pool.lastRewardBlock = block.number;
-        pool.fee = _fee;
-        pool.accTokenPerShare = 0;
-        pools.push(pool);
+    constructor(address _devFeeAddress) {
+        setDevFeeAddress(_devFeeAddress);
     }
 
     // View function to see pending tokens on frontend.
-    function pendingTokens(uint256 _poolID, address _user) public view returns (uint256) {
-        UserInfo storage user = users[_user];
-        uint256 accTokenPerShare = pools[_poolID].accTokenPerShare;
-        uint256 supply = pools[_poolID].token.balanceOf(address(this));
+    function pendingTokens(uint256 _poolID, address _userAddress) external view returns (uint256) {
+        PoolInfo storage pool = pools[_poolID];
+        UserInfo storage user = users[_poolID][_userAddress];
+        uint256 accTokenPerShare = pool.accTokenPerShare;
+        uint256 supply = pool.tokenDeposit.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && supply != 0) {
             uint256 multiplier =  block.number - pool.lastRewardBlock;
-            accTokenPerShare += (multiplier * pools[_poolID].tokensEarnPerBlock) * 10**12 / supply;
+            accTokenPerShare += (multiplier * pool.tokensEarnPerBlock) * 10**12 / supply;
         }
         return user.amount * accTokenPerShare / 10**12 - user.rewardDebt;
     }
 
+    function updateAllPools() public {
+        for (uint256 poolID = 0; poolID < pools.length; ++poolID) updatePool(poolID);
+    }
+
     // Update reward variables of the given pool to be up-to-date.
     function updatePool(uint256 _poolID) public {
-        if (block.number <= pools[_poolID].lastRewardBlock) return;
-        uint256 supply = pools[_poolID].token.balanceOf(address(this));
+        PoolInfo storage pool = pools[_poolID];
+        if (block.number <= pool.lastRewardBlock) return;
+        uint256 supply = pool.tokenEarn.balanceOf(address(this));
         if (supply == 0) {
-            pools[_poolID].lastRewardBlock = block.number;
+            pool.lastRewardBlock = block.number;
             return;
         }
         uint256 multiplier = block.number - pools[_poolID].lastRewardBlock;
         // TODO: Prepay this contract with some tokens and add lines checking that there still enough tokens in contract instead of minting.
-        // TODO: check if we have enough tokens in contract
-        //token.mint(address(this), tokenReward);
-        pools[_poolID].accTokenPerShare += (multiplier * pools[_poolID].tokensEarnPerBlock) * 10**12 / supply;
-        pools[_poolID].lastRewardBlock = block.number;
+
+        // uint256 tokenReward = multiplier * tokenPerBlock;
+        // token.mint(address(this), tokenReward);
+        pool.accTokenPerShare += (multiplier * pool.tokensEarnPerBlock) * 10**12 / supply;
+        pool.lastRewardBlock = block.number;
     }
 
     // Deposit tokens to Pool for token allocation.
     function deposit(uint256 _poolID, uint256 _amount) public nonReentrant {
-        UserInfo storage user = userInfo[msg.sender];
+        require(started, 'deposit: Staking not started yet.');
+        PoolInfo storage pool = pools[_poolID];
+        UserInfo storage user = users[_poolID][msg.sender];
         updatePool(_poolID);
         if (user.amount > 0) {
-            uint256 pending = user.amount * pools[_poolID].accTokenPerShare / 10**12 - user.rewardDebt;
-            if (pending > 0) safeTokenTransfer(msg.sender, pending);
+            uint256 pending = user.amount * pool.accTokenPerShare / 10**12 - user.rewardDebt;
+            if (pending > 0) safeTokenTransfer(address(pool.tokenEarn), msg.sender, pending);
         }
         if (_amount > 0) {
-            pools[_poolID].token.transferFrom(address(msg.sender), address(this), _amount);
-            user.amount = user.amount + _amount;
+            pool.tokenDeposit.safeTransferFrom(address(msg.sender), address(this), _amount);
+            if (pool.fee > 0) {
+                uint256 depositFee = _amount * pool.fee / 10000;
+                pool.tokenDeposit.safeTransfer(devFeeAddress, depositFee);
+                user.amount += _amount - depositFee;
+            } else user.amount += _amount;
         }
-        user.rewardDebt = user.amount * pools[_poolID].accTokenPerShare / 10**12;
+        user.rewardDebt = user.amount * pool.accTokenPerShare / 10**12;
         emit eventDeposit(msg.sender, _poolID, _amount);
     }
 
     // Withdraw tokens from Pool.
     function withdraw(uint256 _poolID, uint256 _amount) public nonReentrant {
-        UserInfo storage user = users[msg.sender];
+        PoolInfo storage pool = pools[_poolID];
+        UserInfo storage user = users[_poolID][msg.sender];
         require(user.amount >= _amount, 'withdraw: Amount is too big');
         updatePool(_poolID);
         uint256 pending = user.amount * pool.accTokenPerShare / 10**12 - user.rewardDebt;
-        if (pending > 0) safeTokenTransfer(msg.sender, pending);
+        if (pending > 0) safeTokenTransfer(address(pool.tokenEarn), msg.sender, pending);
         if (_amount > 0) {
             user.amount = user.amount - _amount;
-            pool.token.transfer(address(msg.sender), _amount);
+            pool.tokenDeposit.safeTransfer(address(msg.sender), _amount);
         }
         user.rewardDebt = user.amount * pool.accTokenPerShare / 10**12;
-        emit eventWithdraw(msg.sender, _amount);
+        emit eventWithdraw(msg.sender, _poolID, _amount);
     }
 
     // Withdraw without caring about rewards. EMERGENCY ONLY.
     function emergencyWithdraw(uint256 _poolID) public nonReentrant {
-        UserInfo storage user = users[msg.sender];
+        PoolInfo storage pool = pools[_poolID];
+        UserInfo storage user = users[_poolID][msg.sender];
         uint256 amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
-        pools[_poolID].token.transfer(address(msg.sender), amount);
-        emit eventEmergencyWithdraw(msg.sender, amount);
+        pool.tokenDeposit.safeTransfer(address(msg.sender), amount);
+        emit eventEmergencyWithdraw(msg.sender, _poolID, amount);
     }
 
     // Safe token transfer function, just in case if rounding error causes pool to not have enough tokens.
-    function safeTokenTransfer(address _to, uint256 _amount) internal {
+    function safeTokenTransfer(address _tokenAddress, address _to, uint256 _amount) internal {
+        IERC20 token = IERC20(_tokenAddress);
         uint256 tokenBal = token.balanceOf(address(this));
         bool transferSuccess = false;
         if (_amount > tokenBal) transferSuccess = token.transfer(_to, tokenBal);
         else transferSuccess = token.transfer(_to, _amount);
         require(transferSuccess, 'safeTokenTransfer: transfer failed');
+    }
+
+    function createPool(address _tokenDepositAddress, address _tokenEarnAddress, uint256 _tokensEarnPerBlock, uint16 _fee) public onlyOwner {
+        PoolInfo memory pool = PoolInfo(
+            IERC20(_tokenDepositAddress),
+            IERC20(_tokenEarnAddress),
+            _tokensEarnPerBlock,
+            0,
+            0,
+            _fee
+        );
+        pools.push(pool);
+    }
+
+    function start() public onlyOwner {
+        updateAllPools();
+        started = true;
+    }
+
+    function setDevFeeAddress(address _devFeeAddress) public onlyOwner {
+        devFeeAddress = _devFeeAddress;
+        emit eventSetDevFeeAddress(msg.sender, _devFeeAddress);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ const Airdrop = artifacts.require('Airdrop');
 const Pool = artifacts.require('Pool');
 
 module.exports = async function(deployer) {
- const token = {address: '0x9b6452d8EE8B79605F3F73d04F5f43D7A9Df59A3'};
- const tokenUSD = '0xF42a4429F107bD120C5E42E069FDad0AC625F615';
+ const tokenOur = {address: '0x9b6452d8EE8B79605F3F73d04F5f43D7A9Df59A3'};
+ const tokenUSD = {address: '0xF42a4429F107bD120C5E42E069FDad0AC625F615'};
  // const router = '0x10ED43C718714eb63d5aA57B78B54704E256024E'; // pancakeswap.finance (BSC Mainnet)
  // const router = '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3'; // pancake.kiemtienonline360.com (BSC Testnet)
  // const router = '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff'; // quickswap.exchange (Polygon Mainnet)
@@ -17,31 +17,35 @@ module.exports = async function(deployer) {
  const presalePriceLiquidity = '2000000000000000000'; // 2 USD
  const presaleDepositTime = '600'; // 10 minutes
  const presaleClaimTime = '300'; // 5 minutes
- const presaleTokenTheirMax = '5000000000000000000000000'; // 5 000 000 USD
- const poolTokensPerBlock = '100000000000000000'; // 0.1 tokens / block
- const tokenName = 'Test token';
- const tokenSymbol = 'TEST';
- const tokenSupply = 10000000; // 10 000 000 tokens
- const tokenDecimals = 18;
- const tokenBurnFee = 2;
- const tokenDevFee = 3;
+ const presaleTokenOurTheirMax = '5000000000000000000000000'; // 5 000 000 USD
+ const poolTokensOurPerBlock = '100000000000000000'; // 0.1 tokens / block
+ const poolTokensUSDPerBlock = '200000000000000000'; // 0.2 tokens / block
+ const poolTokensOurLPPerBlock = '300000000000000000'; // 0.3 tokens / block
+ const tokenOurName = 'Test token';
+ const tokenOurSymbol = 'TEST';
+ const tokenOurSupply = 10000000; // 10 000 000 tokens
+ const tokenOurDecimals = 18;
+ const tokenOurBurnFee = 2;
+ const tokenOurDevFee = 3;
  // const tokenLiquidityFee = 3; // not used yet
 
- // await deployer.deploy(Token, tokenName, tokenSymbol, tokenSupply, tokenDecimals, tokenDevFee, tokenBurnFee);
- // const token = await Token.deployed();
- // await deployer.deploy(Presale, token.address, tokenUSD, router, dev, presalePricePresale, presalePriceLiquidity, presaleDepositTime, presaleClaimTime, presaleTokenTheirMax);
+ // await deployer.deploy(Token, tokenOurName, tokenOurSymbol, tokenOurSupply, tokenOurDecimals, tokenOurDevFee, tokenOurBurnFee);
+ // const tokenOur = await Token.deployed();
+ // await deployer.deploy(Presale, tokenOur.address, tokenUSD, router, dev, presalePricePresale, presalePriceLiquidity, presaleDepositTime, presaleClaimTime, presaleTokenTheirMax);
  // const presale = await Presale.deployed();
- // await deployer.deploy(Airdrop, token.address, airdropAmount);
+ // await deployer.deploy(Airdrop, tokenOur.address, airdropAmount);
  // const airdrop = await Airdrop.deployed();
- await deployer.deploy(Pool, token.address, poolTokensPerBlock);
+ await deployer.deploy(Pool);
  const pool = await Pool.deployed();
  // airdrop.start(airdropTime);
- // pool.createPool(tokenUSD, token.address, );
+ pool.createPool(tokenOur.address, tokenOur.address, poolTokensOurPerBlock, 0); // Our -> Our
+ pool.createPool(tokenUSD.address, tokenOur.address, poolTokensUSDPerBlock, 4); // BUSD -> Our
+ pool.createPool(tokenOurLPAddress, tokenOur.address, poolTokensOurLPPerBlock, 0); // Our-BUSD -> Our
 
  // TODO: THE FOLLOWING TOKEN FUNCTIONS WORK ONLY IF A NEW TOKEN IS DEPLOYED, NOT WITH JUST ADDRESS
- //token.setTaxExclusion(airdrop.address, true);
- //token.setTaxExclusion(presale.address, true);
- //token.transfer(presale.address, '7500000000000000000000000'); // 7 500 000
- //token.transfer(airdrop.address, '500000000000000000000000'); // 500 000
- //token.transfer(pool.address, '2000000000000000000000000'); // 2 000 000
+ //tokenOur.setTaxExclusion(airdrop.address, true);
+ //tokenOur.setTaxExclusion(presale.address, true);
+ //tokenOur.transfer(presale.address, '7500000000000000000000000'); // 7 500 000
+ //tokenOur.transfer(airdrop.address, '500000000000000000000000'); // 500 000
+ //tokenOur.transfer(pool.address, '2000000000000000000000000'); // 2 000 000
 };
