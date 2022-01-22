493551a415d7f8405deade01eb4d992cc02a637b
AloeSwap <info@aloeswap.com>
2022-01-23 00:21:02 +0100

    TODO

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,8 @@ const Airdrop = artifacts.require('Airdrop');
 const Pool = artifacts.require('Pool');
 
 module.exports = async function(deployer) {
- // const tokenOur = {address: '0x9b6452d8EE8B79605F3F73d04F5f43D7A9Df59A3'};
- // tokenOurLPAddress = '0xA4A52Ef4f83bfb5fC9661d6B558e144CAC0f1242';
+ const tokenOur = {address: '0x9b6452d8EE8B79605F3F73d04F5f43D7A9Df59A3'};
+ const tokenOurLPAddress = '0xA4A52Ef4f83bfb5fC9661d6B558e144CAC0f1242';
  const tokenUSD = {address: '0xF42a4429F107bD120C5E42E069FDad0AC625F615'};
  // const routerAddress = '0x10ED43C718714eb63d5aA57B78B54704E256024E'; // pancakeswap.finance (BSC Mainnet)
  // const routerAddress = '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3'; // pancake.kiemtienonline360.com (BSC Testnet)
@@ -18,7 +18,7 @@ module.exports = async function(deployer) {
  const presalePriceLiquidity = '2000000000000000000'; // 2 USD
  const presaleDepositTime = '600'; // 10 minutes
  const presaleClaimTime = '300'; // 5 minutes
- const presaleTokenOurTheirMax = '5000000000000000000000000'; // 5 000 000 USD
+ const presaleTokenTheirMax = '5000000000000000000000000'; // 5 000 000 USD
  const poolTokensOurPerBlock = '100000000000000000'; // 0.1 tokens / block
  const poolTokensUSDPerBlock = '200000000000000000'; // 0.2 tokens / block
  const poolTokensOurLPPerBlock = '300000000000000000'; // 0.3 tokens / block
@@ -29,11 +29,11 @@ module.exports = async function(deployer) {
  const tokenOurBurnFee = 2;
  const tokenOurDevFee = 3;
 
- await deployer.deploy(Token, tokenOurName, tokenOurSymbol, tokenOurSupply, tokenOurDecimals, tokenOurDevFee, tokenOurBurnFee, routerAddress, tokenUSD.address);
- const tokenOur = await Token.deployed();
- const tokenOurLPAddress = await tokenOur.getPairAddress();
- // await deployer.deploy(Presale, tokenOur.address, tokenUSD.address, routerAddress, devAddress, presalePricePresale, presalePriceLiquidity, presaleDepositTime, presaleClaimTime, presaleTokenTheirMax);
- // const presale = await Presale.deployed();
+ //await deployer.deploy(Token, tokenOurName, tokenOurSymbol, tokenOurSupply, tokenOurDecimals, tokenOurDevFee, tokenOurBurnFee, routerAddress, tokenUSD.address);
+ //const tokenOur = await Token.deployed();
+ //const tokenOurLPAddress = await tokenOur.getPairAddress();
+ await deployer.deploy(Presale, tokenOur.address, tokenUSD.address, routerAddress, devAddress, presalePricePresale, presalePriceLiquidity, presaleDepositTime, presaleClaimTime, presaleTokenTheirMax);
+ const presale = await Presale.deployed();
  // await deployer.deploy(Airdrop, tokenOur.address, airdropAmount);
  // const airdrop = await Airdrop.deployed();
  //await deployer.deploy(Pool, devAddress);
