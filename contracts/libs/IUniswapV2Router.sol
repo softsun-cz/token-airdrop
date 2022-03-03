a9f242eea20c80ad94fd6a2f6d76b4d17cb1f878
AloeSwap <info@aloeswap.com>
2022-03-03 14:26:17 +0100

    hardhat config

--- contracts/libs/IUniswapV2Router.sol
+++ contracts/libs/IUniswapV2Router.sol
@@ -4,23 +4,7 @@ pragma solidity ^0.8.0;
 
 interface IUniswapV2Router {
   function factory() external view returns (address);
-
-  function addLiquidity(
-    address tokenA,
-    address tokenB,
-    uint256 amountADesired,
-    uint256 amountBDesired,
-    uint256 amountAMin,
-    uint256 amountBMin,
-    address to,
-    uint256 deadline
-  )
-    external
-    returns (
-      uint256 amountA,
-      uint256 amountB,
-      uint256 liquidity
-    );
+  function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, address to, uint deadline) external returns (uint amountA, uint amountB, uint liquidity);
 }
 
 contract UniswapV2RouterMock is IUniswapV2Router {
@@ -34,23 +18,7 @@ contract UniswapV2RouterMock is IUniswapV2Router {
     return factoryAddress;
   }
 
-  function addLiquidity(
-    address tokenA,
-    address tokenB,
-    uint256 amountADesired,
-    uint256 amountBDesired,
-    uint256 amountAMin,
-    uint256 amountBMin,
-    address to,
-    uint256 deadline
-  )
-    external
-    returns (
-      uint256 amountA,
-      uint256 amountB,
-      uint256 liquidity
-    )
-  {
+  function addLiquidity(address tokenA, address tokenB, uint amountADesired, uint amountBDesired, uint amountAMin, uint amountBMin, address to, uint deadline) external returns (uint amountA, uint amountB, uint liquidity) {
     return (1, 1, 1);
   }
 }
